d174bb8f51b8501918d1483536eeaacf0c4f88ca
Nabeel-Mehmood <nabeelmehmood0714@gmail.com>
2025-04-04 16:19:19 +0500

    updated user profile schema

--- HerbScan_Backend/models/User.js
+++ HerbScan_Backend/models/User.js
@@ -1,9 +1,15 @@
+// models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  blocked: { type: Boolean, default: false },
+  age: { type: Number, default: null },
+  contact: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  profileImage: { type: String, default: 'default_profile.jpg' }
 });
 
 module.exports = mongoose.model('User', UserSchema);
