89d7f6c0930a65504afedbac8f584b89e258f9af
Nabeel-Mehmood <nabeelmehmood0714@gmail.com>
2025-04-08 15:50:06 +0500

    Updated home.js

--- HerbScan_Frontend/HerbScan_Website/src/Component/home.js
+++ HerbScan_Frontend/HerbScan_Website/src/Component/home.js
@@ -1,22 +1,8 @@
 import React, { useState } from 'react';
 import './home.css';
 import './header.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/autoplay';
-import { Autoplay } from 'swiper/modules';
-import { register } from 'swiper/element';
 import Header from '../Component/header'; 
 import Footer from '../Component/footer'; 
-
-// Images for the slider
-import image1 from '../Assets/image1.jpg';
-import image2 from '../Assets/image2.jpg';
-import image3 from '../Assets/image3.jpg';
-import image4 from '../Assets/image4.jpg';
-import image5 from '../Assets/image5.jpg';
-
-register();
 
 function Home() {
   const [uploadedImage, setUploadedImage] = useState(null);
@@ -61,6 +47,7 @@ function Home() {
   return (
     <div className="home-container">
       <Header showSearchBar={true} />
+
       <div className="upload-section">
         <div
           className="file-upload"
@@ -107,62 +94,16 @@ function Home() {
           )}
         </div>
       </div>
-      <div className="info-slider-section content-section">
+
+      <div className="info-section content-section">
         <div className="website-info">
           <h2>About HerbScan</h2>
           <p>
             HerbScan helps you identify different trees by uploading images. Use our powerful tool to learn more about the flora around you.
           </p>
         </div>
-        <div className="image-slider">
-          <Swiper
-            modules={[Autoplay]}
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            spaceBetween={30}
-            slidesPerView={1}
-            loop={true}
-            pagination={{
-              clickable: true,
-              bulletClass: 'swiper-pagination-bullet',
-              bulletActiveClass: 'swiper-pagination-bullet-active',
-            }}
-          >
-            <SwiperSlide>
-              <div className="slide-container">
-                <img src={image1} alt="Slide 1" />
-                <div className="caption">Caption for Slide 1</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-container">
-                <img src={image2} alt="Slide 2" />
-                <div className="caption">Caption for Slide 2</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-container">
-                <img src={image3} alt="Slide 3" />
-                <div className="caption">Caption for Slide 3</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-container">
-                <img src={image4} alt="Slide 4" />
-                <div className="caption">Caption for Slide 4</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-container">
-                <img src={image5} alt="Slide 5" />
-                <div className="caption">Caption for Slide 5</div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
       </div>
+
       <Footer />
     </div>
   );
